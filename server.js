10d45bd25338750a4e6e6c9a574f27030a880292
Glitch (wheat-piano) <none>
2018-03-26 05:12:42 +0000

    🏂🐢 Checkpoint ./server.js:774464/2206

--- server.js
+++ server.js
@@ -1,43 +1,49 @@
+// server.js
+// where your node app starts
 
-//Basic required import for NodeJS
+// init project
 var express = require('express')
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var bodyParser = require('body-parser')
+var cors = require('cors')
 
-//Create an instance of express for our app and instantiate bodyParser and Cors
-var app = express();
+
+var app = module.exports = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
-
-//Get CALL to return JSON thats format natural and unix date
-app.get("/:dateVal", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
+app.get('/dateValues/:dateVal', function(req,res,next){
+  //takes in request data
+  var dateVal = req.params.dateVal;
   
-  var dateVal = request.params.dateVal;
-  
-  var dateFormattingOption = {
-  year :"numeric",
-  month :"long",
-  day :"numeric"
+  var dateFormattingOptions = {
+  year: 'numeric',
+  month: 'long',
+  date: 'numeric'
   };
   
   if(isNaN(dateVal)){
   var naturalDate = new Date(dateVal);
-  naturalDate = naturalDate.toLocalDateString("en-us",dateFormattingOption);
-  var unixDate = new Date(dateVal).getTime()/1000;
+  naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);
+  var unixDate = new Date(dateVal).getTime()/1000; 
   }
-   response.json({unix: unixDate,natural: naturalDate});
+  else{
+  var unixDate = dateVal;
+  var naturalDate = new Date(dateVal * 1000);   
+  naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);    
+  }
+  
+  res.json({unix: unixDate, natural: naturalDate});
+
+  
+  
 });
 
 
-
-// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
