9f502ffd507c7718fc7d2695ff78e487f8c49e96
Glitch (wheat-piano) <none>
2018-03-26 04:21:02 +0000

    💝📷 Checkpoint ./server.js:774464/1521

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-//Bsic require import nodejs
-var express = require("express");
-var cors = require("cors");
-var bodyParser = require("body-parser");
+// server.js
+// where your node app starts
 
-//create instance of express and instantiate bodyParser and cors
-var app = module.exports = express();
-app.use = (bodyParser.json());
-app.use = (cors());
+// init project
+const express = require('express')
+const app = express()
 
-app.get("/dateValues/:dateVal",function(req,res,next){
-var dateVal = req.params.dateVal;
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-req.json({unix:dateVal});
-});
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
 
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
 
-app.listen(3000,function(){
-  console.log("WHATS");
-});
+// Simple in-memory store
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+]
+
+app.get("/dreams", (request, response) => {
+  response.send(dreams)
+})
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", (request, response) => {
+  dreams.push(request.query.dream)
+  response.sendStatus(200)
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
